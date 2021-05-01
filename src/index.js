62bb87fcba43070fc4a28440db7661ffab22c0d9
helitsa <elisa.piola91@gmail.com>
2021-05-01 17:37:11 +0200

    I've rounded the hours and minutes on JS

--- src/index.js
+++ src/index.js
@@ -4,7 +4,13 @@ let h2 = document.querySelector("li");
 
 let date = now.getDate();
 let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
 let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
 
 let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 let day = days[now.getDay()];
