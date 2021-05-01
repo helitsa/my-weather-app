c7b46b874974d8176b0b3d1e68fc1d8cc48fe1fe
helitsa <elisa.piola91@gmail.com>
2021-05-01 18:01:45 +0200

    The icon now changes according to the forecast

--- src/index.js
+++ src/index.js
@@ -47,7 +47,7 @@ function displayWeathercondition(response) {
 
   let iconElement=document.querySelector("#icon");
   iconElement.setAttribute(
-    "src","http://openweathermap.org/img/wn/10d@2x.png"
+    "src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
 }
 
