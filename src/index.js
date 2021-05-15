e93554c4b92930384c8fa76d285c361fe8c55616
helitsa <elisa.piola91@gmail.com>
2021-05-15 15:25:51 +0200

    removed the Imperial conversion

--- src/index.js
+++ src/index.js
@@ -35,16 +35,16 @@ h2.innerHTML = `${day} ${date} ${month}, ${hours}:${minutes}`;
 
 
 function getForecast(coordinates){
-  console.log(coordinates);
+  
   let apiKey = "595772f32b2e7808bc60ef54e46fcfa0";
   let apiUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  console.log(apiUrl);
+ 
   axios.get(apiUrl).then(displayForecast);
 }
 
 function displayWeathercondition(response) {
   document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(celsiusTemperature);
+  document.querySelector("#temperature").innerHTML = Math.round(response.data.main.temp);
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
@@ -142,33 +142,6 @@ forecastHTML= forecastHTML + `</div>`;
 }
 
 
-
-
-function displayFahrenheitTemperature(event){
-  event.preventDefault();
-  let temperatureElement= document.querySelector("#temperature");
-  celsiusLink.classList.remove("active");
-  fahrenheitLink.classList.add("active");
-  let fahrenheitTemperature= (celsiusTemperature * 9)/5+32;
-temperatureElement.innerHTML=Math.round(fahrenheitTemperature);
-}
-
-function displayCelsiusTemperature(event){
-  event.preventDefault();
-  celsiusLink.classList.add("active");
-  fahrenheitLink.classList.remove("active");
-  let temperatureElement= document.querySelector("#temperature");
-  temperatureElement.innerHTML= Math.round(celsiusTemperature);
-}
-
-let celsiusTemperature= null;
-
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
-let celsiusLink = document.querySelector("#celsius-link");
-celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 
 searchCity("New York");
